7c091dff41256c2c521b086305cb73d3790771e8
Wawa755 <146659430+Wawa755@users.noreply.github.com>
2024-06-08 01:34:37 +0200

    Last update frfr

--- javascript.js
+++ javascript.js
@@ -1,64 +1,66 @@
-/*HOT DEALS--------------------------------------------------------------------------*/
-const slides = document.querySelectorAll('.slides');
-const prevBtn = document.getElementById('prev-btn');
-const nextBtn = document.getElementById('next-btn');
-const dots = document.querySelectorAll('.dot');
-
-
-let index = 0;
-
-// Adding opacity to first dot on first time
-
-dots[0].style.backgroundColor = '#F35E00'; // Set initial active color
-
-// positioning the slides
-
-slides.forEach((slide,index)=>{
- slide.style.left=`${index*100}%`;
-});
-
-
-// move slide function
-
-const moveSlide = () =>{
- slides.forEach((slide)=>{
-  slide.style.transform=`translateX(-${index*100}%)`;
- });
-};
-
-// remove dots background color from all dots
-
-const removeDotsActiveColor = () =>{
- dots.forEach((dot)=>{
-  dot.style.backgroundColor = '#D9D9D9'; // Set inactive color
- });
-};
-
-dots.forEach((dot,i)=>{
- dot.addEventListener("click",(e)=>{
- index=i;
- removeDotsActiveColor();
- e.target.style.backgroundColor = '#F35E00'; // Set active color
- moveSlide();
- });
-});
-
-// show the previous slide
-
-slides.forEach((slide, index) => {
-    slide.style.left = `${index * 100}%`;
-  });
-
-// auto play slide
-
-/*CATEGORIES--------------------------------------------------------------------------*/
 document.addEventListener('DOMContentLoaded', () => {
-    if (document.querySelector('.Cat_cards')) {
-        // Slider dragging
-        const slider = document.querySelector('.Cat_cards');
+
+    /*HOT DEALS--------------------------------------------------------------------------*/
+
+    const slides = document.querySelectorAll('.slides');
+    const dots = document.querySelectorAll('.dot');
+    let index = 0;
+
+    dots[0].style.backgroundColor = '#F35E00'; // Set initial active color
+
+    slides.forEach((slide, index) => {
+        slide.style.left = `${index * 100}%`;
+    });
+
+    const moveSlide = () => {
+        slides.forEach((slide) => {
+            slide.style.transform = `translateX(-${index * 100}%)`;
+        });
+    };
+
+    const removeDotsActiveColor = () => {
+        dots.forEach((dot) => {
+            dot.style.backgroundColor = '#D9D9D9'; // Set inactive color
+        });
+    };
+
+    dots.forEach((dot, i) => {
+        dot.addEventListener("click", (e) => {
+            index = i;
+            removeDotsActiveColor();
+            e.target.style.backgroundColor = '#F35E00'; // Set active color
+            moveSlide();
+        });
+    });
+
+    /*CATEGORIES--------------------------------------------------------------------------*/
+    
+    const setupSlider = (sliderSelector) => {
+        const slider = document.querySelector(sliderSelector);
+        if (!slider) return;
+
         let isDown = false;
         let startX;
         let scrollLeft;
+        let velX = 0;
+        let momentumID;
+
+        const cancelMomentumTracking = () => {
+            cancelAnimationFrame(momentumID);
+        };
+
+        const beginMomentumTracking = () => {
+            cancelMomentumTracking();
+            momentumID = requestAnimationFrame(momentumLoop);
+        };
+
+        const momentumLoop = () => {
+            slider.scrollLeft += velX * 2;
+            velX *= 0.95;
+            if (Math.abs(velX) > 0.5) {
+                momentumID = requestAnimationFrame(momentumLoop);
+            }
+        };
 
         slider.addEventListener('mousedown', (e) => {
             isDown = true;
@@ -83,200 +85,25 @@ document.addEventListener('DOMContentLoaded', () => {
             if (!isDown) return;
             e.preventDefault();
             const x = e.pageX - slider.offsetLeft;
-            const walk = (x - startX); //scroll-fast
-            var prevScrollLeft = slider.scrollLeft;
+            const walk = x - startX; //scroll-fast
+            const prevScrollLeft = slider.scrollLeft;
             slider.scrollLeft = scrollLeft - walk;
             velX = slider.scrollLeft - prevScrollLeft;
         });
 
-        // Momentum 
-        var velX = 0;
-        var momentumID;
-
-        slider.addEventListener('wheel', (e) => {
+        slider.addEventListener('wheel', () => {
             cancelMomentumTracking();
         });
 
-        function beginMomentumTracking() {
-            cancelMomentumTracking();
-            momentumID = requestAnimationFrame(momentumLoop);
-        }
-
-        function cancelMomentumTracking() {
-            cancelAnimationFrame(momentumID);
-        }
-
-        function momentumLoop() {
-            slider.scrollLeft += velX * 2;
-            velX *= 0.95;
-            if (Math.abs(velX) > 0.5) {
-                momentumID = requestAnimationFrame(momentumLoop);
-            }
-        }
-
-        // Scroll
-        const scrollContainer = document.querySelector(".Cat_cards");
-
-        scrollContainer.addEventListener("wheel", (evt) => {
+        slider.addEventListener('wheel', (evt) => {
             evt.preventDefault();
-
             window.requestAnimationFrame(() => {
-                scrollContainer.scrollTo({ top: 0, left: scrollContainer.scrollLeft + (evt.deltaY * 2), behavior: "smooth" });
+                slider.scrollTo({ top: 0, left: slider.scrollLeft + (evt.deltaY * 2), behavior: "smooth" });
             });
         });
-    }
-});
+    };
 
-/*SEASONAL--------------------------------------------------------------------------*/
-
-document.addEventListener('DOMContentLoaded', () => {
-    if (document.querySelector('.Sea_Cards ')) {
-        // Slider dragging
-        const slider = document.querySelector('.Sea_Cards ');
-        let isDown = false;
-        let startX;
-        let scrollLeft;
-
-        slider.addEventListener('mousedown', (e) => {
-            isDown = true;
-            slider.classList.add('active');
-            startX = e.pageX - slider.offsetLeft;
-            scrollLeft = slider.scrollLeft;
-            cancelMomentumTracking();
-        });
-
-        slider.addEventListener('mouseleave', () => {
-            isDown = false;
-            slider.classList.remove('active');
-        });
-
-        slider.addEventListener('mouseup', () => {
-            isDown = false;
-            slider.classList.remove('active');
-            beginMomentumTracking();
-        });
-
-        slider.addEventListener('mousemove', (e) => {
-            if (!isDown) return;
-            e.preventDefault();
-            const x = e.pageX - slider.offsetLeft;
-            const walk = (x - startX); //scroll-fast
-            var prevScrollLeft = slider.scrollLeft;
-            slider.scrollLeft = scrollLeft - walk;
-            velX = slider.scrollLeft - prevScrollLeft;
-        });
-
-        // Momentum 
-        var velX = 0;
-        var momentumID;
-
-        slider.addEventListener('wheel', (e) => {
-            cancelMomentumTracking();
-        });
-
-        function beginMomentumTracking() {
-            cancelMomentumTracking();
-            momentumID = requestAnimationFrame(momentumLoop);
-        }
-
-        function cancelMomentumTracking() {
-            cancelAnimationFrame(momentumID);
-        }
-
-        function momentumLoop() {
-            slider.scrollLeft += velX * 2;
-            velX *= 0.95;
-            if (Math.abs(velX) > 0.5) {
-                momentumID = requestAnimationFrame(momentumLoop);
-            }
-        }
-
-        // Scroll
-        const scrollContainer = document.querySelector(".Sea_Cards");
-
-        scrollContainer.addEventListener("wheel", (evt) => {
-            evt.preventDefault();
-
-            window.requestAnimationFrame(() => {
-                scrollContainer.scrollTo({ top: 0, left: scrollContainer.scrollLeft + (evt.deltaY * 2), behavior: "smooth" });
-            });
-        });
-    }
-});
-
-/*RESERVE--------------------------------------------------------------------------*/
-
-document.addEventListener('DOMContentLoaded', () => {
-    if (document.querySelector('.Res_Cards ')) {
-        // Slider dragging
-        const slider = document.querySelector('.Res_Cards ');
-        let isDown = false;
-        let startX;
-        let scrollLeft;
-
-        slider.addEventListener('mousedown', (e) => {
-            isDown = true;
-            slider.classList.add('active');
-            startX = e.pageX - slider.offsetLeft;
-            scrollLeft = slider.scrollLeft;
-            cancelMomentumTracking();
-        });
-
-        slider.addEventListener('mouseleave', () => {
-            isDown = false;
-            slider.classList.remove('active');
-        });
-
-        slider.addEventListener('mouseup', () => {
-            isDown = false;
-            slider.classList.remove('active');
-            beginMomentumTracking();
-        });
-
-        slider.addEventListener('mousemove', (e) => {
-            if (!isDown) return;
-            e.preventDefault();
-            const x = e.pageX - slider.offsetLeft;
-            const walk = (x - startX); //scroll-fast
-            var prevScrollLeft = slider.scrollLeft;
-            slider.scrollLeft = scrollLeft - walk;
-            velX = slider.scrollLeft - prevScrollLeft;
-        });
-
-        // Momentum 
-        var velX = 0;
-        var momentumID;
-
-        slider.addEventListener('wheel', (e) => {
-            cancelMomentumTracking();
-        });
-
-        function beginMomentumTracking() {
-            cancelMomentumTracking();
-            momentumID = requestAnimationFrame(momentumLoop);
-        }
-
-        function cancelMomentumTracking() {
-            cancelAnimationFrame(momentumID);
-        }
-
-        function momentumLoop() {
-            slider.scrollLeft += velX * 2;
-            velX *= 0.95;
-            if (Math.abs(velX) > 0.5) {
-                momentumID = requestAnimationFrame(momentumLoop);
-            }
-        }
-
-        // Scroll
-        const scrollContainer = document.querySelector(".Res_Cards");
-
-        scrollContainer.addEventListener("wheel", (evt) => {
-            evt.preventDefault();
-
-            window.requestAnimationFrame(() => {
-                scrollContainer.scrollTo({ top: 0, left: scrollContainer.scrollLeft + (evt.deltaY * 2), behavior: "smooth" });
-            });
-        });
-    }
+    setupSlider('.Cat_cards');
+    setupSlider('.Sea_Cards');
+    setupSlider('.Res_Cards');
 });
